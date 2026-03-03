422939a59a34539bb1738b887f539ebc1c2c2c45
gauger <gustavojosiasg@gmail.com>
2026-03-02 21:23:15 -0300

    feat: implementado builders aninhados para atualizações granulares da interface

--- lib/app/widgets/cards/categorias_card.dart
+++ lib/app/widgets/cards/categorias_card.dart
@@ -17,7 +17,78 @@ class CategoriasCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListenableBuilder(
       listenable: ranchoViewModel,
-      builder: (context, child) {
+      child: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              categorias.tituloCategoria,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ListenableBuilder(
+              listenable: ranchoViewModel,
+              builder: (context, child) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black26,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white60, width: 1),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ...ranchoViewModel
+                          .getListaItens(categorias)
+                          .take(4)
+                          .map(
+                            (nome) => Padding(
+                              padding: const EdgeInsets.only(bottom: 2),
+                              child: Text(
+                                "• $nome", // Um bullet point ajuda na leitura
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 14,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+
+                      if (ranchoViewModel.getListaItens(categorias).length > 4)
+                        Text(
+                          "+ ${ranchoViewModel.getListaItens(categorias).length - 4} itens...",
+                          style: TextStyle(
+                            color: Colors.deepPurpleAccent,
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                    ],
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(height: 4),
+            // Text(
+            //   'Itens: ${categorias.itens}',
+            //   style: const TextStyle(color: Colors.white, fontSize: 10),
+            //   textAlign: TextAlign.center,
+            // ),
+          ],
+        ),
+      ),
+      builder: (context, staticChild) {
         return Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
@@ -43,80 +114,7 @@ class CategoriasCard extends StatelessWidget {
                 ),
               );
             },
-            child: Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    categorias.tituloCategoria,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  ListenableBuilder(
-                    listenable: ranchoViewModel,
-                    builder: (context, child) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 6,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.black26,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(color: Colors.white60, width: 1),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ...ranchoViewModel
-                                .getListaItens(categorias)
-                                .take(4)
-                                .map(
-                                  (nome) => Padding(
-                                    padding: const EdgeInsets.only(bottom: 2),
-                                    child: Text(
-                                      "• $nome", // Um bullet point ajuda na leitura
-                                      style: TextStyle(
-                                        color: Colors.white70,
-                                        fontSize: 14,
-                                      ),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                ),
-
-                            if (ranchoViewModel
-                                    .getListaItens(categorias)
-                                    .length >
-                                4)
-                              Text(
-                                "+ ${ranchoViewModel.getListaItens(categorias).length - 4} itens...",
-                                style: TextStyle(
-                                  color: Colors.deepPurpleAccent,
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-
-                  const SizedBox(height: 4),
-                  // Text(
-                  //   'Itens: ${categorias.itens}',
-                  //   style: const TextStyle(color: Colors.white, fontSize: 10),
-                  //   textAlign: TextAlign.center,
-                  // ),
-                ],
-              ),
-            ),
+            child: staticChild,
           ),
         );
       },
