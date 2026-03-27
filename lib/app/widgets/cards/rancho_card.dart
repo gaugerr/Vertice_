b444af3f087b6f96df976d7c98c58e2009eb3e51
gauger <gustavojosiasg@gmail.com>
2026-03-26 21:25:17 -0300

    feat: implementação do popupMenuButton personalizado no ranchoCard + posicionamento do icone com Stack/Positioned

--- lib/app/widgets/cards/rancho_card.dart
+++ lib/app/widgets/cards/rancho_card.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:rancho_consciente/app/model/rancho_model.dart';
 import 'package:rancho_consciente/app/view/categorias_view.dart';
 import 'package:rancho_consciente/app/view_model/rancho_viewmodel.dart';
+import 'package:rancho_consciente/app/widgets/confirm_action_dialog.dart';
+import 'package:rancho_consciente/app/widgets/popup_menu_button.dart';
+import 'package:rancho_consciente/app/widgets/rename_action_dialog.dart';
 
 class RanchoCard extends StatelessWidget {
   final RanchoViewModel ranchoViewModel;
@@ -32,36 +35,85 @@ class RanchoCard extends StatelessWidget {
             ),
           );
         },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Text('id = ${rancho.id}'),
-              Text(
-                rancho.mercado,
-
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      rancho.mercado,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      rancho.descricao,
+                      style: const TextStyle(
+                        color: Colors.white60,
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "${rancho.data.day.toString().padLeft(2, '0')}/${rancho.data.month.toString().padLeft(2, '0')}/${rancho.data.year}",
+                      style: const TextStyle(
+                        color: Colors.white24,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 10),
-              Text(
-                rancho.descricao,
-                style: const TextStyle(color: Colors.white60, fontSize: 14),
-                textAlign: TextAlign.center,
+            ),
+
+            Positioned(
+              top: 8,
+              right: 8,
+              child: CustomPopupMenuButton(
+                onExcludePressed: () => showDialog(
+                  context: context,
+                  builder: (context) => ConfirmActionDialog(
+                    title: 'Excluir Lista?',
+                    description: "Deseja remover '${rancho.mercado}'?",
+                    cancelActionLabel: 'CANCELAR',
+                    confirmActionLabel: 'EXCLUIR',
+                    onConfirm: () async =>
+                        await ranchoViewModel.deleteBuyList(rancho),
+                    confirmColor: Colors.red,
+                  ),
+                ),
+                onRenamePressed: () => showDialog(
+                  context: context,
+                  builder: (context) => RenameActionDialog(
+                    renamedController: TextEditingController(
+                      text: rancho.mercado,
+                    ),
+                    title: 'Renomear Lista',
+                    validatorErrorEmpty: 'Digite o nome do Mercado',
+                    validatorErrorMaximumLength: 'Máximo 50 caracteres',
+                    cancelActionLabel: 'CANCELAR',
+                    confirmActionLabel: 'SALVAR',
+                    onConfirm: (novoNome) async {
+                      final itemNovo = rancho.copyWith(mercado: novoNome);
+
+                      await ranchoViewModel.updateBuyList(itemNovo);
+                    },
+                  ),
+                ),
+                excludeLabel: 'Excluir',
+                renameLabel: 'Renomear',
               ),
-              const SizedBox(height: 8),
-              Text(
-                //  rancho.data.toString(),
-                "${rancho.data.day.toString().padLeft(2, '0')}/${rancho.data.month.toString().padLeft(2, '0')}/${rancho.data.year}",
-                style: const TextStyle(color: Colors.white24, fontSize: 12),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
